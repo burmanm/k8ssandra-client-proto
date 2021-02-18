9a9fc4270be3e51b0f1135b31505740a86898165
Michael Burman <yak@iki.fi>
2021-02-18 14:02:29 +0200

    Fix nodetool command

--- cmd/kubectl-k8ssandra/nodetool/nodetool.go
+++ cmd/kubectl-k8ssandra/nodetool/nodetool.go
@@ -6,9 +6,12 @@ import (
 
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/kubectl/pkg/cmd/exec"
+	"k8s.io/kubectl/pkg/scheme"
 )
 
 var (
@@ -16,18 +19,18 @@ var (
 	# launch a interactive cqlsh shell on node
 	%[1]s nodetool <pod> <command> [<args>]
 `
-	errNoPodDefined     = fmt.Errorf("no target pod defined, could not execute cqlsh")
-	errNoCommandDefined = fmt.Errorf("nodetool requires command to execute")
+	errNotEnoughParameters = fmt.Errorf("not enough parameters to run nodetool")
 )
 
 type options struct {
+	namespace   string
 	configFlags *genericclioptions.ConfigFlags
 	genericclioptions.IOStreams
-	namespace   string
 	podName     string
 	nodeCommand string
 	args        []string
 	execOptions *exec.ExecOptions
+	restConfig  *rest.Config
 }
 
 func newOptions(streams genericclioptions.IOStreams) *options {
@@ -62,30 +65,35 @@ func NewCmd(streams genericclioptions.IOStreams) *cobra.Command {
 	}
 
 	o.configFlags.AddFlags(cmd.Flags())
-
-	// func (p *ExecOptions) Complete(f cmdutil.Factory, cmd *cobra.Command, argsIn []string, argsLenAtDash int) error {
-
 	return cmd
+}
+
+// setKubernetesDefaults hack is copied from kubectl/exec (unexported)
+func setKubernetesDefaults(config *rest.Config) error {
+	// TODO remove this hack.  This is allowing the GetOptions to be serialized.
+	config.GroupVersion = &schema.GroupVersion{Group: "", Version: "v1"}
+
+	if config.APIPath == "" {
+		config.APIPath = "/api"
+	}
+	if config.NegotiatedSerializer == nil {
+		// This codec factory ensures the resources are not converted. Therefore, resources
+		// will not be round-tripped through internal versions. Defaulting does not happen
+		// on the client.
+		config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	}
+	return rest.SetKubernetesDefaults(config)
 }
 
 // Complete parses the arguments and necessary flags to options
 func (c *options) Complete(cmd *cobra.Command, args []string) error {
 	var err error
 
-	c.namespace, err = cmd.Flags().GetString("namespace")
-	if err != nil {
-		return err
+	if len(args) < 2 {
+		return errNotEnoughParameters
 	}
 
-	if len(args) > 0 {
-		// We just ignore if there's more than 1 pod parameter
-		c.podName = args[0]
-	}
-
-	if len(args) > 1 {
-		c.nodeCommand = args[1]
-	}
-
+	c.nodeCommand = args[1]
 	if len(args) > 2 {
 		c.args = args[2:]
 	}
@@ -98,13 +106,13 @@ func (c *options) Complete(cmd *cobra.Command, args []string) error {
 		Executor: &exec.DefaultRemoteExecutor{},
 	}
 
-	execOptions.PodName = c.podName
+	execOptions.PodName = args[0]
 
-	execOptions.Command = []string{"nodetool", "--username X", "--password Y", "status"}
 	execOptions.Namespace, execOptions.EnforceNamespace, err = c.configFlags.ToRawKubeConfigLoader().Namespace()
 	if err != nil {
 		return err
 	}
+	c.namespace = execOptions.Namespace
 
 	execOptions.ContainerName = "cassandra"
 
@@ -113,10 +121,14 @@ func (c *options) Complete(cmd *cobra.Command, args []string) error {
 	// 	return err
 	// }
 
-	execOptions.Config, err = c.configFlags.ToRESTConfig()
+	c.restConfig, err = c.configFlags.ToRESTConfig()
 	if err != nil {
 		return err
 	}
+
+	// Hack from kubectl's exec. Probably a bug in the Kubernetes' client implementation
+	setKubernetesDefaults(c.restConfig)
+	execOptions.Config = c.restConfig
 
 	clientset, err := kubernetes.NewForConfig(execOptions.Config)
 	if err != nil {
@@ -126,107 +138,54 @@ func (c *options) Complete(cmd *cobra.Command, args []string) error {
 
 	c.execOptions = execOptions
 
-	// kubectl exec mypod -c ruby-container -i -t -- bash -il
+	// Create the correct command line here
+
+	user, pass, _ := c.getUserNamePassword()
+	execOptions.Command = []string{"nodetool", "--username", user, "--password", pass, c.nodeCommand}
+	if len(c.args) > 0 {
+		execOptions.Command = append(execOptions.Command, c.args...)
+	}
 
 	return nil
 }
-
-/*
-func (p *ExecOptions) Complete(f cmdutil.Factory, cmd *cobra.Command, argsIn []string, argsLenAtDash int) error {
-	if len(argsIn) > 0 && argsLenAtDash != 0 {
-		p.ResourceName = argsIn[0]
-	}
-	if argsLenAtDash > -1 {
-		p.Command = argsIn[argsLenAtDash:]
-	} else if len(argsIn) > 1 {
-		fmt.Fprint(p.ErrOut, "kubectl exec [POD] [COMMAND] is DEPRECATED and will be removed in a future version. Use kubectl exec [POD] -- [COMMAND] instead.\n")
-		p.Command = argsIn[1:]
-	} else if len(argsIn) > 0 && len(p.FilenameOptions.Filenames) != 0 {
-		fmt.Fprint(p.ErrOut, "kubectl exec [POD] [COMMAND] is DEPRECATED and will be removed in a future version. Use kubectl exec [POD] -- [COMMAND] instead.\n")
-		p.Command = argsIn[0:]
-		p.ResourceName = ""
-	}
-
-	var err error
-	p.Namespace, p.EnforceNamespace, err = f.ToRawKubeConfigLoader().Namespace()
-	if err != nil {
-		return err
-	}
-
-	p.ExecutablePodFn = polymorphichelpers.AttachablePodForObjectFn
-
-	p.GetPodTimeout, err = cmdutil.GetPodRunningTimeoutFlag(cmd)
-	if err != nil {
-		return cmdutil.UsageErrorf(cmd, err.Error())
-	}
-
-	p.Builder = f.NewBuilder
-	p.restClientGetter = f
-
-	cmdParent := cmd.Parent()
-	if cmdParent != nil {
-		p.ParentCommandName = cmdParent.CommandPath()
-	}
-	if len(p.ParentCommandName) > 0 && cmdutil.IsSiblingCommandExists(cmd, "describe") {
-		p.EnableSuggestedCmdUsage = true
-	}
-
-	p.Config, err = f.ToRESTConfig()
-	if err != nil {
-		return err
-	}
-
-	clientset, err := f.KubernetesClientSet()
-	if err != nil {
-		return err
-	}
-	p.PodClient = clientset.CoreV1()
-
-	return nil
-}
-*/
 
 // Validate ensures that all required arguments and flag values are provided
 func (c *options) Validate() error {
-	if c.podName == "" {
-		return errNoPodDefined
-	}
-
-	if c.nodeCommand == "" {
-		return errNoCommandDefined
-	}
-
 	// We could validate here if a nodetool command requires flags, but lets let nodetool throw that error
 
 	return nil
 }
 
-// Run triggers the nodetool command on target pod
-func (c *options) Run() error {
+func (c *options) getUserNamePassword() (string, string, error) {
 	// TODO Could be a util .. I guess I'll use this often - move also a lot of the logic to the pkg
-	config, err := c.configFlags.ToRESTConfig()
+	clientset, err := kubernetes.NewForConfig(c.restConfig)
 	if err != nil {
-		return err
+		return "", "", err
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
+	// TODO Hack for now, this requires a patch in k8ssandra
 	secret, err := clientset.CoreV1().Secrets(c.namespace).Get(context.TODO(), "demo-reaper-secret-k8ssandra", metav1.GetOptions{})
 	if err != nil {
-		return err
+		return "", "", err
 	}
 
 	user := secret.Data["username"]
 	pass := secret.Data["password"]
 
-	fmt.Printf("Found: %v %v\n", string(user), string(pass))
+	return string(user), string(pass), nil
+}
+
+// Run triggers the nodetool command on target pod
+func (c *options) Run() error {
 
 	// TODO Set container to "cassandra", (-c cassandra)
 	// kubectl exec pod -c cassandra ..
 	// -- command part should be the after nodetool part
 
-	return c.execOptions.Run()
+	err := c.execOptions.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
